435eea45802fe3efc5aff60643098520a0279873
王刚 <wanggang@317hu.com>
2019-04-12 11:29:49 +0800

    登录按钮动画

--- Swift/WGLoginBtnDemo/WGLoginBtnDemo/AppDelegate.swift
+++ Swift/WGLoginBtnDemo/WGLoginBtnDemo/AppDelegate.swift
@@ -15,7 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow.init(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.white
+        
+        let vc = ViewController.init()
+        window?.rootViewController = vc
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
--- Swift/WGLoginBtnDemo/WGLoginBtnDemo/ViewController.swift
+++ Swift/WGLoginBtnDemo/WGLoginBtnDemo/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var loginBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "登录"
+        view.backgroundColor = UIColor.white
+        
+        //动画的起始阶段:loginBtn初始位置设置在屏幕外
+        loginBtn = UIButton.init(frame: CGRect.init(x: -view.frame.width, y: 200, width: view.frame.width-20*2, height: 50))
+        loginBtn.backgroundColor = UIColor.yellow
+        loginBtn.setTitle("登录", for: .normal)
+        loginBtn.setTitleColor(UIColor.black, for: .normal)
+        view.addSubview(loginBtn)
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
+        //动画的进行阶段:恢复loginBtn正确位置
+        //viewDidAppear中设置的话,视图已经显示,会有不连贯
+        UIView.animate(withDuration: 1) {
 
+            self.loginBtn.frame = CGRect.init(x: 20, y: self.loginBtn.frame.origin.y, width: self.loginBtn.frame.size.width, height: self.loginBtn.frame.size.height)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        UIView.animate(withDuration: 1, animations: {
+
+            self.loginBtn.frame = CGRect.init(x: -self.view.frame.width, y: self.loginBtn.frame.origin.y, width: self.loginBtn.frame.size.width, height: self.loginBtn.frame.size.height)
+
+        }) { (isTrue) in
+
+            UIView.animate(withDuration: 2) {
+
+                self.loginBtn.frame = CGRect.init(x: 20, y: self.loginBtn.frame.origin.y, width: self.loginBtn.frame.size.width, height: self.loginBtn.frame.size.height)
+            }
+        }
+    }
 }
 
