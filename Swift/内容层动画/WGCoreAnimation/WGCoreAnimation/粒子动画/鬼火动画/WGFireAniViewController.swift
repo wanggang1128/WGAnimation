232d101829b9d3ec6835f5b6acf76b3de9df8d32
王刚 <wanggang@317hu.com>
2019-04-25 15:13:54 +0800

    大火动画

--- Swift/内容层动画/WGCoreAnimation/WGCoreAnimation/粒子动画/鬼火动画/WGFireAniViewController.swift
+++ Swift/内容层动画/WGCoreAnimation/WGCoreAnimation/粒子动画/鬼火动画/WGFireAniViewController.swift
@@ -21,7 +21,7 @@ class WGFireAniViewController: UIViewController {
         //粒子发射位置
         emitterLay.position = self.view.center
         // 控制火苗大小
-        emitterLay.emitterSize = CGSize(width: 100, height: 50)
+        emitterLay.emitterSize = CGSize(width: WgWith, height: 30)
         emitterLay.renderMode = .additive
         // 控制发射源模式 即形状
         emitterLay.emitterMode = .outline
@@ -33,21 +33,21 @@ class WGFireAniViewController: UIViewController {
         //发射方向
         emitterCell.emissionLongitude = CGFloat(Double.pi)
         // 粒子速度 负数表明向上燃烧
-        emitterCell.velocity = -100
+        emitterCell.velocity = -10
         // 粒子速度范围
         emitterCell.velocityRange = 20
         //周围发射角度
-        emitterCell.emissionRange = 2
+        emitterCell.emissionRange = 1.5
         //粒子y方向的加速度分量
-        emitterCell.yAcceleration = -200
+        emitterCell.yAcceleration = -100
         //缩放比例 超大火苗
-        emitterCell.scaleSpeed = 0.5
+        emitterCell.scaleSpeed = 1.2
         //火苗颜色
         emitterCell.color = UIColor(red: 0.8 ,green: 0.4 ,blue:0.2 ,alpha:0.1).cgColor
         emitterCell.contents = UIImage(named: "fire.png")!.cgImage
         
         emitterLay.emitterCells = [emitterCell];
-        emitterLay.setValue(1000, forKeyPath: "emitterCells.fire.birthRate")
+        emitterLay.setValue(500, forKeyPath: "emitterCells.fire.birthRate")
         emitterLay.setValue(1, forKeyPath: "emitterCells.fire.lifetime")
         
     }
